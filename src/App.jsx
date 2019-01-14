b00bd0c360569e61290875398e010160422628c7
Alex <ajhollid@gmail.com>
2019-01-13 22:37:26 -0800

    Removed bootstrap module, add bootstrap v3 from CDN for react compatability

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,44 @@ import ReactBootstrapSlider from 'react-bootstrap-slider';
 
 
 class App extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      sliderValue: 5,
+    };
+    this.onSliderChange = this.onSliderChange.bind( this );
+  }
+
+  onSliderChange( e ) {
+    this.setState( {
+      sliderValue: e.target.value,
+    } );
+  }
+
   render() {
-    console.log( 'wtf' );
+    const { sliderValue } = this.state;
     return (
       <div>
         <Grid fluid>
           <Row className="show-grid">
-            <Col xs={12} md={2}>
-              <code>
+            <Col xs={6} md={2}>
+
+              <Row className="show-grid">
                 {/* {'<Col xs={12} md={2} />'} */}
-                <ReactBootstrapSlider
-                  value={5}
-                  min={0}
-                  max={10}
-                  step={1}
-                  enabled="enabled"
-                />
-              </code>
+                <Col xs={6} md={10}>
+                  <ReactBootstrapSlider
+                    value={sliderValue}
+                    change={this.onSliderChange}
+                    min={0}
+                    max={10}
+                    step={1}
+                    enabled="enabled"
+                  />
+                </Col>
+                <Col xs={6} md={2}>
+                  <p>{sliderValue}</p>
+                </Col>
+              </Row>
             </Col>
             <Col xs={6} md={10}>
               <code>{'<Col xs={6} md={10} />'}</code>
--- src/Index.jsx
+++ src/Index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import 'bootstrap/dist/css/bootstrap.css';
 
 require( '../styles/application.scss' );
 
